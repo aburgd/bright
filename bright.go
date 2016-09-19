3bfcdc79f70e1ab6906ee2b571e066cf63f58580
Alec Burgdorf <aeburgd@gmail.com>
2016-09-19 22:50:32 +0000

    refactor some bits

--- bright.go
+++ bright.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+  // "github.com/joho/godotenv"
   "github.com/go-chat-bot/bot/irc"
   _ "github.com/aburgd/plugins/gif"
   _ "github.com/aburgd/plugins/catgif"
   _ "github.com/aburgd/plugins/catfacts"
   _ "github.com/aburgd/plugins/treta"
-  _ "github.com/aburgd/plugins/url"
   _ "github.com/aburgd/plugins/puppet"
   // custom plugins
   _ "github.com/aburgd/plugins/hello"
@@ -20,19 +20,24 @@ import (
 )
 
 func main()  {
+  // err := godotenv.Load()
+  // if err != nil {
+  //  log.Fatal("Error loading .env")
+  // }
 
-  server := os.Getenv("IRC_SERVER")
-  channels := strings.Split("#" + os.Getenv("IRC_CHANNELS"), ",")
   // load the config
   config := brightConfig()
-  log.Printf("connected to server: %s; channels: %s", server, channels)
+  log.Printf("connected to server: %s\n channels: %s", config.Server, config.Channels)
   irc.Run(config)
 }
 
 func brightConfig() *irc.Config {
+  server := os.Getenv("IRC_SERVER")
+  channels := strings.Split("#" + os.Getenv("IRC_CHANNELS"), ",")
+
   return &irc.Config{
-		Server:   os.Getenv("IRC_SERVER"),
-		Channels: strings.Split("#" + os.Getenv("IRC_CHANNELS"), ","),
+		Server:   server, //os.Getenv("IRC_SERVER"),
+		Channels: channels, //strings.Split("#" + os.Getenv("IRC_CHANNELS"), ","),
 		User:     os.Getenv("IRC_USER"),
 		Nick:     os.Getenv("IRC_NICK"),
 		Password: os.Getenv("IRC_PASSWORD"),
